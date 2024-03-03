0e5c662456efb3b7af78f808d1a007a5607c4159
alisher22062005 <156660415+alisher22062005@users.noreply.github.com>
2024-03-03 19:42:00 +0600

    Add files via upload

---
+++ Lab6_part1/exercise1.py
@@ -0,0 +1,7 @@
+import os 
+print(os.listdir(r"C:\Users\acer-\OneDrive\Рабочий стол\Lab6_python\file_handing.py"))#print  files and directories
+
+print(os.listdir())#print files except folder except file_handing.py
+
+
+
